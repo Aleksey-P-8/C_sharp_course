7821844d43f1261e6d1584b559e4b142cfcb7413
Aleksey <vpupkin@company.com>
2022-06-27 22:01:48 +0300

    Text

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, all people!");
+﻿// string num_str = Console.ReadLine();
+// int num = int.Parse(num_str)
+
+int num = int.Parse(Console.ReadLine())
+
+Console.WriteLine(num);
+
