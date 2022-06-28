724207ea2716ce977e69c052563027e5c68e8b28
Popov Alexey <alexey.popov@UD.unidraft.com>
2022-06-28 19:48:16 +0300

    1_1дз

---
+++ 1_Homework/1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿string s_n_1 = Console.ReadLine();
+string s_n_2 = Console.ReadLine();
+
+int n_1 = int.Parse(s_n_1);
+int n_2 = int.Parse(s_n_2);
+
+if (n_1 > n_2)
+{
+
+    Console.Write($"The number {n_1} больше чем {n_2}");
+}
+else if (n_1 == n_2)
+{
+    Console.Write($"The number {n_1} равно числу {n_2}");
+}
+else 
+{
+    Console.Write($"The number {n_2} больше чем {n_1}");
+}
