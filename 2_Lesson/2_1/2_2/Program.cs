d9d91381ee91dc2ec03622f68d6207592d12ecc8
Aleksey <vpupkin@company.com>
2022-07-02 16:03:55 +0300

    2_4

--- 2_Lesson/2_1/2_2/Program.cs
+++ 2_Lesson/2_1/2_2/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int Maхnumber(int num)
+{
+    if (num % 10 > num / 10)
+    {
+        return num % 10;
+    }
+    else
+        return num / 10;
+}
+int SecondNum = new Random().Next(10, 100);
+Maхnumber(SecondNum);
+
+Console.WriteLine (SecondNum);
+Console.WriteLine (Maхnumber(SecondNum));
