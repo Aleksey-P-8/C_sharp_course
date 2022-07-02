260b777239a929fa195b8b25d3dd4785dd1cbe31
Aleksey <vpupkin@company.com>
2022-07-02 18:30:34 +0300

    ДЗ1_3

--- 2_Lesson/HW/1_3/Program.cs
+++ 2_Lesson/HW/1_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 99)
+    {
+        Console.WriteLine("нет 3 цифр");
+        return;
+    }
+
+    while (num > 999) num = num / 10;
+    Console.WriteLine(num % 10);
+}
+
+ThirdNum(int.Parse(Console.ReadLine()));
